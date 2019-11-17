9153b128b6338c734db0172836ce95df9ea47334
Malcolm Chaney <chaneym@gmail.com>
2019-11-16 19:31:00 -0800

    Update anjunabot-db.sql

--- db-anjunabot/anjunabot-db.sql
+++ db-anjunabot/anjunabot-db.sql
@@ -1,81 +1,283 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema anjunabot-dev
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema anjunabot-dev
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `anjunabot-dev` DEFAULT CHARACTER SET latin1 ;
+USE `anjunabot-dev` ;
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`message_stage`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`message_stage` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`message_stage` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `path` VARCHAR(512) NOT NULL,
+  `path_type` VARCHAR(48) NULL,
+  `first_name` VARCHAR(64) NOT NULL,
+  `last_name` VARCHAR(64) NULL,
+  `text` VARCHAR(1024) NOT NULL,
+  `chat_id` INT NOT NULL,
+  `chat_title` VARCHAR(128) NOT NULL,
+  `platform` VARCHAR(48) NOT NULL,
+  `date_added` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 36
+DEFAULT CHARACTER SET = latin1;
 
 
--- Dumping database structure for anjunabot
-CREATE DATABASE IF NOT EXISTS `anjunabot` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `anjunabot`;
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`person`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`person` ;
 
--- Dumping structure for table anjunabot.comment
-CREATE TABLE IF NOT EXISTS `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `body_text` varchar(50000) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`person` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(64) NULL,
+  `last_name` VARCHAR(64) NULL,
+  `state` VARCHAR(64) NULL,
+  `date_added` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = latin1;
 
--- Data exporting was unselected.
 
--- Dumping structure for table anjunabot.message_stage
-CREATE TABLE IF NOT EXISTS `message_stage` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` varchar(255) NOT NULL DEFAULT '0',
-  `person` varchar(255) NOT NULL DEFAULT '0',
-  `comment` varchar(5000) NOT NULL DEFAULT '0',
-  `chat_id` varchar(5000) NOT NULL DEFAULT '0',
-  `chat_title` varchar(5000) NOT NULL DEFAULT '0',
-  `platform` varchar(255) NOT NULL DEFAULT '0',
-  `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`chat`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`chat` ;
 
--- Data exporting was unselected.
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`chat` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cid` INT NULL,
+  `title` VARCHAR(128) NULL,
+  `date_added` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
--- Dumping structure for table anjunabot.person
-CREATE TABLE IF NOT EXISTS `person` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) DEFAULT '0',
-  `last_name` varchar(255) DEFAULT '0',
-  `nick_name` varchar(255) DEFAULT '0',
-  `state` varchar(255) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`path`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`path` ;
 
--- Dumping structure for table anjunabot.post
-CREATE TABLE IF NOT EXISTS `post` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `person_id` int(11) NOT NULL DEFAULT '0',
-  `comment_id` int(11) NOT NULL DEFAULT '0',
-  `song_id` int(11) NOT NULL DEFAULT '0',
-  `room` varchar(255) NOT NULL DEFAULT '0',
-  `date_posted` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`path` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(48) NULL,
+  `uri` VARCHAR(512) NULL,
+  `platform` VARCHAR(45) NULL,
+  `date_added` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`post`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`post` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`post` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `chat_id` INT NULL,
+  `path_id` INT NULL,
+  `person_id` INT NULL,
+  `text` VARCHAR(1024) NOT NULL,
+  `date_added` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `fk_post_person` (`person_id`),
-  KEY `fk_post_comment` (`comment_id`),
-  KEY `fk_post_song` (`song_id`),
-  CONSTRAINT `fk_post_comment` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`),
-  CONSTRAINT `fk_post_person` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`),
-  CONSTRAINT `fk_post_song` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  INDEX `fk_post_chat1_idx` (`chat_id` ASC),
+  INDEX `fk_post_person1_idx` (`person_id` ASC),
+  INDEX `fk_post_path1_idx` (`path_id` ASC),
+  CONSTRAINT `fk_post_chat1`
+    FOREIGN KEY (`chat_id`)
+    REFERENCES `anjunabot-dev`.`chat` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_person1`
+    FOREIGN KEY (`person_id`)
+    REFERENCES `anjunabot-dev`.`person` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_path1`
+    FOREIGN KEY (`path_id`)
+    REFERENCES `anjunabot-dev`.`path` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
--- Data exporting was unselected.
 
--- Dumping structure for table anjunabot.song
-CREATE TABLE IF NOT EXISTS `song` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT '0',
-  `artist` varchar(255) DEFAULT '0',
-  `spotify_url` varchar(255) DEFAULT '0',
-  `youtube_url` varchar(255) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`track`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`track` ;
 
--- Data exporting was unselected.
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`track` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `path_id` INT NULL,
+  `title` VARCHAR(256) NULL,
+  `date_added` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_track_path1_idx` (`path_id` ASC),
+  CONSTRAINT `fk_track_path1`
+    FOREIGN KEY (`path_id`)
+    REFERENCES `anjunabot-dev`.`path` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`album`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`album` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`album` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(256) NULL,
+  `type` VARCHAR(64) NULL,
+  `num_tracks` INT NULL,
+  `artist_id` INT NULL,
+  `track_id` INT NULL,
+  `path_id` INT NULL,
+  `date_added` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_album_path1_idx` (`path_id` ASC),
+  CONSTRAINT `fk_album_path1`
+    FOREIGN KEY (`path_id`)
+    REFERENCES `anjunabot-dev`.`path` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`artist`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`artist` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`artist` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `path_id` INT NULL,
+  `name` VARCHAR(256) NULL,
+  `date_added` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_artist_path1_idx` (`path_id` ASC),
+  CONSTRAINT `fk_artist_path1`
+    FOREIGN KEY (`path_id`)
+    REFERENCES `anjunabot-dev`.`path` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`track_artists`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`track_artists` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`track_artists` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `track_id` INT NULL,
+  `artist_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_track_artists_track1_idx` (`track_id` ASC),
+  INDEX `fk_track_artists_artist1_idx` (`artist_id` ASC),
+  CONSTRAINT `fk_track_artists_track1`
+    FOREIGN KEY (`track_id`)
+    REFERENCES `anjunabot-dev`.`track` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_track_artists_artist1`
+    FOREIGN KEY (`artist_id`)
+    REFERENCES `anjunabot-dev`.`artist` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`album_tracks`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`album_tracks` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`album_tracks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `album_id` INT NULL,
+  `track_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_album_tracks_track1_idx` (`track_id` ASC),
+  INDEX `fk_album_tracks_album1_idx` (`album_id` ASC),
+  CONSTRAINT `fk_album_tracks_track1`
+    FOREIGN KEY (`track_id`)
+    REFERENCES `anjunabot-dev`.`track` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_album_tracks_album1`
+    FOREIGN KEY (`album_id`)
+    REFERENCES `anjunabot-dev`.`album` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`playlist`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`playlist` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`playlist` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `path_id` INT NULL,
+  `title` VARCHAR(256) NULL,
+  `date_added` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_playlist_path1_idx` (`path_id` ASC),
+  CONSTRAINT `fk_playlist_path1`
+    FOREIGN KEY (`path_id`)
+    REFERENCES `anjunabot-dev`.`path` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `anjunabot-dev`.`playlist_tracks`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `anjunabot-dev`.`playlist_tracks` ;
+
+CREATE TABLE IF NOT EXISTS `anjunabot-dev`.`playlist_tracks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `playlist_id` INT NULL,
+  `track_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_playlist_tracks_playlist1_idx` (`playlist_id` ASC),
+  INDEX `fk_playlist_tracks_track1_idx` (`track_id` ASC),
+  CONSTRAINT `fk_playlist_tracks_playlist1`
+    FOREIGN KEY (`playlist_id`)
+    REFERENCES `anjunabot-dev`.`playlist` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_playlist_tracks_track1`
+    FOREIGN KEY (`track_id`)
+    REFERENCES `anjunabot-dev`.`track` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
